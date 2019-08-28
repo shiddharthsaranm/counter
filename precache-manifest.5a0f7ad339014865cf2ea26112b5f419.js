4594bac7a715fbb99e9ecd1cf5e77e4e1c5f5c82
Shiddharth Saran Marudachalam <shiddharth.16cs@kct.ac.in>
2019-08-28 19:55:12 +0530

    Updates

---
+++ precache-manifest.5a0f7ad339014865cf2ea26112b5f419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8def8af8de52c4031eb6d685ab08e39a",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "58e6c862b93f0a0322fc",
+    "url": "/counter/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7588096149e94c083425",
+    "url": "/counter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "58e6c862b93f0a0322fc",
+    "url": "/counter/static/js/2.36ebb900.chunk.js"
+  },
+  {
+    "revision": "7588096149e94c083425",
+    "url": "/counter/static/js/main.aff888a2.chunk.js"
+  },
+  {
+    "revision": "4d25a895ed3f11370198",
+    "url": "/counter/static/js/runtime~main.3a77d02d.js"
+  }
+]);
